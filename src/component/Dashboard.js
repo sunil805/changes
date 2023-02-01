3bec01afa0fda9d3a44e2cad965ba390adce09dd
sunil805 <124151369+sunil805@users.noreply.github.com>
2023-02-01 21:23:25 +0530

    changes

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -18,14 +18,18 @@ import {
   faRobot,
   faAppleAlt,
   faXmark,
+  faBars
+  
 } from "@fortawesome/free-solid-svg-icons";
 import img from "../Asset/stats.png";
 import moment from "moment";
 
 const Dashboard = () => {
+
   const [data, setdata] = useState();
   const [change, setchange] = useState(false);
   const[select , setselect] = useState(10)
+  const [button , setbutton] = useState()
   const [state, setState] = useState([
     {
       startDate: new Date(),
@@ -35,7 +39,7 @@ const Dashboard = () => {
   ]);
 
   useEffect(() => {
-    const data = () => fetch(
+    const data = fetch(
       `https://admindevapi.wowtalent.live/api/admin/dashboard/installstatasticlist?fromdate=${moment(
         state[0]?.startDate
       ).format("YYYY-MM-DD")}&todate=${moment(state[0]?.endDate).format(
@@ -44,11 +48,11 @@ const Dashboard = () => {
     )
       .then((res) => res.json())
       .then((res) => setdata(res));
-    data()
   }, [state , select]);
   return (
-    <div className="admin_page w-full h-full flex justify-between">
-      <div className="left_content ">
+    <div className="admin_page w-full h-full flex items-baseline justify-between lg:flex-row flex-col">
+      <FontAwesomeIcon icon={faBars} className = "lg:hidden block pl-3 pt-5 text-white z-10 " onClick={()=>setbutton(!button)} />
+      <div className="left_content hidden lg:block">
         <div className="bg-[#283046] p-3 w-[220px] h-[80vh] ">
           <div className="flex justify-between">
             <h1 className="text-blue-900 text-2xl font-[800]">WOW</h1>
@@ -104,11 +108,72 @@ const Dashboard = () => {
           </ul>
         </div>
       </div>
-      <div className="right_content flex  w-full mt-5 gap-5 flex-col ml-7 ">
-        <div className="bg-[#283046] w-[1250px]  h-[238px] ">
-          <div className="flex items-center w-full justify-center pt-10 pl-10 ">
+
+
+      {
+!button ? ""  : <div className="left_content absolute">
+<div className="bg-[#283046] p-3 w-[220px] h-[80vh] ">
+  <div className="flex justify-between ">
+    <h1 className="text-blue-900 text-2xl font-[800] w-full pl-5">WOW</h1>
+    <img alt="" src={img} className="w-[20px]" />
+  </div>
+  <ul className="mt-4 text-white  text-sm cursor-pointer">
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+      <FontAwesomeIcon icon={faDashboard} />
+      <p className="font-[500]">Dashboard</p>
+    </li>
+
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+      <FontAwesomeIcon icon={faUser} />
+      <p className="font-[500]">WOW Users</p>
+    </li>
+
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+      <FontAwesomeIcon icon={faVideoCamera} />
+      <p className="font-[500]">Video Clips</p>
+    </li>
+
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+      <FontAwesomeIcon icon={faTriangleExclamation} />
+      <p className="font-[500]">Reported Content</p>
+    </li>
+
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+      <FontAwesomeIcon icon={faDashboard} />
+      <p className="font-[500]">Category</p>
+    </li>
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg  items-center">
+      <FontAwesomeIcon icon={faFileCircleExclamation} />
+      <p className="font-[500]">info page</p>
+    </li>
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+      <FontAwesomeIcon icon={faDashboard} />
+      <p className="font-[500]">FAQ</p>
+    </li>
+
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+      <FontAwesomeIcon icon={faBell} />
+      <p className="font-[500]">Push Notifications</p>
+    </li>
+
+    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+      <div className="flex  space-x-7">
+        <FontAwesomeIcon icon={faUserPlus} />
+
+        <p className="font-[500]">Internal User</p>
+      </div>
+      <FontAwesomeIcon icon={faAngleRight} />
+    </li>
+  </ul>
+</div>
+</div>
+
+      }
+      <div className="right_content flex  w-full mt-5 gap-5 flex-col lg:ml-7 ml-0 lg:p-0  px-4  ">
+        <div className="bg-[#283046] lg:w-[1250px] w-full lg:h-[238px] h-[80vh] ">
+          <div className="flex items-center w-full justify-center lg:pt-10 pt-5 lg:pl-10 p-5 ">
             <div className="flex flex-col w-full ">
-              <div className=" flex justify-between w-[80%]">
+              <div className=" flex justify-between lg:w-[80%] w-full  lg:flex-row flex-col space-y-5">
                 <div className="flex space-x-5 items-center">
                   <div className="w-[63px] h-[63px] bg-white flex items-center justify-center rounded-full">
                     <FontAwesomeIcon
@@ -116,7 +181,7 @@ const Dashboard = () => {
                       className="w-[36px] h-[36px]"
                     />
                   </div>
-                  <div className="">
+                  <div className="w-full">
                     <div className="font-[20px]  text-white">3154</div>
                     <span className="font-[10px] text-white">
                       App installed
@@ -125,7 +190,7 @@ const Dashboard = () => {
                 </div>
                 <div className="flex space-x-5 items-center">
                   <div className="w-[63px] h-[63px] bg-white flex items-center justify-center rounded-full"></div>
-                  <div className="">
+                  <div className="w-full">
                     <div className="font-[20px]  text-white">900</div>
                     <span className="font-[10px] text-white">
                       Active installed
@@ -134,14 +199,14 @@ const Dashboard = () => {
                 </div>
                 <div className="flex space-x-5 items-center">
                   <div className="w-[63px] h-[63px] bg-white flex items-center justify-center rounded-full"></div>
-                  <div className="">
+                  <div className="w-full">
                     <div className="font-[20px]  text-white">14.85%</div>
                     <span className="font-[10px] text-white">Churn Rate</span>
                   </div>
                 </div>
               </div>
 
-              <div className=" flex justify-between w-[80%] mt-5">
+              <div className=" flex justify-between w-full mt-5 lg:w-[80%]   lg:flex-row flex-col space-y-5">
                 <div className="flex space-x-5 items-center">
                   <div className="w-[63px] h-[63px] bg-white flex items-center justify-center rounded-full">
                     <FontAwesomeIcon
@@ -149,7 +214,7 @@ const Dashboard = () => {
                       className="w-[36px] h-[36px]"
                     />
                   </div>
-                  <div className="">
+                  <div className="w-full">
                     <div className="font-[20px]  text-white">3154</div>
                     <span className="font-[10px] text-white">
                       App installed
@@ -158,7 +223,7 @@ const Dashboard = () => {
                 </div>
                 <div className="flex space-x-5 items-center">
                   <div className="w-[63px] h-[63px] bg-white flex items-center justify-center rounded-full"></div>
-                  <div className="">
+                  <div className="w-full">
                     <div className="font-[20px]  text-white">900</div>
                     <span className="font-[10px] text-white">
                       Active installed
@@ -167,7 +232,7 @@ const Dashboard = () => {
                 </div>
                 <div className="flex space-x-5 items-center">
                   <div className="w-[63px] h-[63px] bg-white flex items-center justify-center rounded-full"></div>
-                  <div className="">
+                  <div className="w-full">
                     <div className="font-[20px]  text-white">14.85%</div>
                     <span className="font-[10px] text-white">Churn Rate</span>
                   </div>
@@ -177,7 +242,7 @@ const Dashboard = () => {
           </div>
         </div>
 
-        <div className="bg-[#283046] w-[1250px] p-2">
+        <div className="bg-[#283046] lg:w-[1250px] w-full p-2">
           <div className=" flex justify-between">
             <div className="space-x-4 capitalize ">
               <span className="text-white">show</span>
@@ -227,7 +292,7 @@ const Dashboard = () => {
 
           <div>
             <div className=" bg-[#271C1C]  border border-gray-500  mt-4 ">
-              <ul className="  flex justify-between py-2 pl-10 text-[12px]  text-white w-[90%]">
+              <ul className="  flex justify-between py-2 lg:pl-10 p-2 lg:text-[12px]  text-[5px] text-white lg:w-[90%] w-full">
                 <li>Day</li>
                 <li>Days install</li>
                 <li>plateform</li>
