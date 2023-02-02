ad5d92d5eed463665baf69c79dce7cb1a8bd01c6
sunil805 <124151369+sunil805@users.noreply.github.com>
2023-02-02 09:08:23 +0530

    changes

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -5,7 +5,6 @@ import "react-date-range/dist/theme/default.css";
 import { addDays } from "date-fns";
 import { DateRangePicker } from "react-date-range";
 import {
-  
   faDashboard,
   faUser,
   faVideoCamera,
@@ -18,22 +17,20 @@ import {
   faRobot,
   faAppleAlt,
   faXmark,
-  faBars
-  
+  faBars,
 } from "@fortawesome/free-solid-svg-icons";
 import img from "../Asset/stats.png";
 import moment from "moment";
-
-import ReactPaginate from 'react-paginate'
+// import ReactDOM from 'react-dom';
+import ReactPaginate from "react-paginate";
 
 const Dashboard = () => {
-
   const [data, setdata] = useState();
   const [change, setchange] = useState(false);
-  const[select , setselect] = useState(10)
-  const [button , setbutton] = useState();
-  const [page , setPage] = useState(1)
-  const [pageCount , setPageCount] = useState(0)
+  const [select, setselect] = useState(10);
+  const [button, setbutton] = useState();
+  const [page, setPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
   const [state, setState] = useState([
     {
       startDate: new Date(),
@@ -43,28 +40,32 @@ const Dashboard = () => {
   ]);
 
   useEffect(() => {
-    const data = ()=>fetch(
-      `https://admindevapi.wowtalent.live/api/admin/dashboard/installstatasticlist?fromdate=${moment(
-        state[0]?.startDate
-      ).format("YYYY-MM-DD")}&todate=${moment(state[0]?.endDate).format(
-        "YYYY-MM-DD"
-      )}&page=${page}&limit=${select}`
-    )
-      .then((res) => res.json())
-      .then((res) => {
-        setdata(res);
-        setPageCount(res?.data?.pages ,"jhshs")
-      });
-      data()
-  }, [state , select , page]);
+    const data = () =>
+      fetch(
+        `https://admindevapi.wowtalent.live/api/admin/dashboard/installstatasticlist?fromdate=${moment(
+          state[0]?.startDate
+        ).format("YYYY-MM-DD")}&todate=${moment(state[0]?.endDate).format(
+          "YYYY-MM-DD"
+        )}&page=${page}&limit=${select}`
+      )
+        .then((res) => res.json())
+        .then((res) => {
+          setdata(res);
+          setPageCount(res?.data?.pages, "jhshs");
+        });
+    data();
+  }, [state, select, page]);
 
-
-const handlePageClick = (e) =>{
-  setPage(e?.selected + 1)
-}
+  const handlePageClick = (e) => {
+    setPage(e?.selected + 1);
+  };
   return (
     <div className="admin_page w-full h-full flex items-baseline justify-between lg:flex-row flex-col lg:items-start">
-      <FontAwesomeIcon icon={faBars} className = "lg:hidden block pl-3 pt-5 text-white z-10 " onClick={()=>setbutton(!button)} />
+      <FontAwesomeIcon
+        icon={faBars}
+        className="lg:hidden block pl-3 pt-5 text-white z-10 "
+        onClick={() => setbutton(!button)}
+      />
       <div className="left_content hidden lg:block">
         <div className="bg-[#283046] p-3 w-[220px] h-[80vh] ">
           <div className="flex justify-between">
@@ -122,71 +123,73 @@ const handlePageClick = (e) =>{
         </div>
       </div>
 
+      {!button ? (
+        ""
+      ) : (
+        <div className="left_content absolute">
+          <div className="bg-[#283046] p-3 w-[220px] h-[100vh] ">
+            <div className="flex justify-between ">
+              <h1 className="text-blue-900 text-2xl font-[800] w-full pl-5">
+                WOW
+              </h1>
+              <img alt="" src={img} className="w-[20px]" />
+            </div>
+            <ul className="mt-4 text-white  text-sm cursor-pointer">
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+                <FontAwesomeIcon icon={faDashboard} />
+                <p className="font-[500]">Dashboard</p>
+              </li>
 
-      {
-!button ? ""  : <div className="left_content absolute">
-<div className="bg-[#283046] p-3 w-[220px] h-[100vh] ">
-  <div className="flex justify-between ">
-    <h1 className="text-blue-900 text-2xl font-[800] w-full pl-5">WOW</h1>
-    <img alt="" src={img} className="w-[20px]" />
-  </div>
-  <ul className="mt-4 text-white  text-sm cursor-pointer">
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
-      <FontAwesomeIcon icon={faDashboard} />
-      <p className="font-[500]">Dashboard</p>
-    </li>
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+                <FontAwesomeIcon icon={faUser} />
+                <p className="font-[500]">WOW Users</p>
+              </li>
 
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
-      <FontAwesomeIcon icon={faUser} />
-      <p className="font-[500]">WOW Users</p>
-    </li>
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+                <FontAwesomeIcon icon={faVideoCamera} />
+                <p className="font-[500]">Video Clips</p>
+              </li>
 
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
-      <FontAwesomeIcon icon={faVideoCamera} />
-      <p className="font-[500]">Video Clips</p>
-    </li>
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+                <FontAwesomeIcon icon={faTriangleExclamation} />
+                <p className="font-[500]">Reported Content</p>
+              </li>
 
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
-      <FontAwesomeIcon icon={faTriangleExclamation} />
-      <p className="font-[500]">Reported Content</p>
-    </li>
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+                <FontAwesomeIcon icon={faDashboard} />
+                <p className="font-[500]">Category</p>
+              </li>
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg  items-center">
+                <FontAwesomeIcon icon={faFileCircleExclamation} />
+                <p className="font-[500]">info page</p>
+              </li>
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+                <FontAwesomeIcon icon={faDashboard} />
+                <p className="font-[500]">FAQ</p>
+              </li>
 
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
-      <FontAwesomeIcon icon={faDashboard} />
-      <p className="font-[500]">Category</p>
-    </li>
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg  items-center">
-      <FontAwesomeIcon icon={faFileCircleExclamation} />
-      <p className="font-[500]">info page</p>
-    </li>
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
-      <FontAwesomeIcon icon={faDashboard} />
-      <p className="font-[500]">FAQ</p>
-    </li>
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+                <FontAwesomeIcon icon={faBell} />
+                <p className="font-[500]">Push Notifications</p>
+              </li>
 
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
-      <FontAwesomeIcon icon={faBell} />
-      <p className="font-[500]">Push Notifications</p>
-    </li>
+              <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
+                <div className="flex  space-x-7">
+                  <FontAwesomeIcon icon={faUserPlus} />
 
-    <li className="flex space-x-7 hover:bg-blue-500 hover:text-white p-2 rounded-lg items-center">
-      <div className="flex  space-x-7">
-        <FontAwesomeIcon icon={faUserPlus} />
-
-        <p className="font-[500]">Internal User</p>
-      </div>
-      <FontAwesomeIcon icon={faAngleRight} />
-    </li>
-  </ul>
-</div>
-</div>
-
-      }
+                  <p className="font-[500]">Internal User</p>
+                </div>
+                <FontAwesomeIcon icon={faAngleRight} />
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
       <div className="right_content flex  w-full mt-5 gap-5 flex-col lg:ml-7 ml-0 lg:p-0  px-4  ">
         <div className="bg-[#283046] lg:w-[1250px] w-full lg:h-[238px] h-[100vh] ">
           <div className="flex items-center w-full justify-center lg:pt-10 pt-5 lg:pl-10 p-5 ">
-            <div className="flex flex-col w-full ">
-              <div className=" flex justify-between lg:w-[80%] w-full  space-y-5">
+            <div className="flex  w-full items-baseline">
+              <div className=" flex justify-between lg:w-[80%] w-full flex-col  space-y-5">
                 <div className="flex space-x-5 items-center">
                   <div className="w-[109px]   h-[63px] bg-white flex items-center justify-center rounded-full">
                     <FontAwesomeIcon
@@ -219,7 +222,7 @@ const handlePageClick = (e) =>{
                 </div>
               </div>
 
-              <div className=" flex justify-between w-full mt-5 lg:w-[80%]   items-baseline space-y-5">
+              <div className=" flex justify-between w-full mt-5 lg:w-[80%] flex-col   items-baseline space-y-5">
                 <div className="flex space-x-5 items-center">
                   <div className="w-[109px] h-[63px] bg-white flex items-center justify-center rounded-full">
                     <FontAwesomeIcon
@@ -259,7 +262,10 @@ const handlePageClick = (e) =>{
           <div className=" flex justify-between">
             <div className="space-x-4 capitalize ">
               <span className="text-white">show</span>
-              <select className="border border-gray-700 bg-[#161C32] w-16 rounded-md text-white cursor-pointer" onChange={(e)=>setselect(e.target.value)}>
+              <select
+                className="border border-gray-700 bg-[#161C32] w-16 rounded-md text-white cursor-pointer"
+                onChange={(e) => setselect(e.target.value)}
+              >
                 <option value="10">10</option>
                 <option value="50">50</option>
                 <option value="100">100</option>
@@ -315,77 +321,75 @@ const handlePageClick = (e) =>{
                 <li>Churn plateform</li>
               </ul>
             </div>
-          
-          <div>
-          {data?.data?.data?.map((data, index) => {
-              return (
-                <>
-                <div className="  text-[5px] md:text-[12px] flex justify-between  lg:w-[90%] w-full mt-4 text-white items-center">
-                  <span>{moment(data.created_At).format("YYYY-MM-DD")}</span>
-                  <span className="w-[30px]">{data.totalinstall}</span>
-                  <div className="flex flex-col items-baseline w-[80px]">
-                    <div className="space-x-2">
-                      <FontAwesomeIcon icon={faRobot} />
-                      <span>{data.android_install}</span>
-                    </div>
-                    <div className="space-x-2">
-                      <FontAwesomeIcon icon={faAppleAlt} />
-                      <span>{data.ios_install}</span>
-                    </div>
-                  </div>
-                  <span className="w-[30px]">{data.totaluninstall}</span>
 
-                  <div className="flex flex-col items-baseline w-[80px]">
-                    <div className="space-x-2">
-                      <FontAwesomeIcon icon={faRobot} />
-                      <span>{data.android_uninstall}</span>
-                    </div>
-                    <div className="space-x-2">
-                      <FontAwesomeIcon icon={faAppleAlt} />
-                      <span>{data.ios_uninstall}</span>
-                    </div>
-                  </div>
+            <div>
+              {data?.data?.data?.map((data, index) => {
+                return (
+                  <>
+                    <div className="  text-[5px] md:text-[12px] flex justify-between  lg:w-[90%] w-full mt-4 text-white items-center">
+                      <span>
+                        {moment(data.created_At).format("YYYY-MM-DD")}
+                      </span>
+                      <span className="w-[30px]">{data.totalinstall}</span>
+                      <div className="flex flex-col items-baseline w-[80px]">
+                        <div className="space-x-2">
+                          <FontAwesomeIcon icon={faRobot} />
+                          <span>{data.android_install}</span>
+                        </div>
+                        <div className="space-x-2">
+                          <FontAwesomeIcon icon={faAppleAlt} />
+                          <span>{data.ios_install}</span>
+                        </div>
+                      </div>
+                      <span className="w-[30px]">{data.totaluninstall}</span>
 
-                  <span className="w-[30px]">{data.totalchurn}</span>
+                      <div className="flex flex-col items-baseline w-[80px]">
+                        <div className="space-x-2">
+                          <FontAwesomeIcon icon={faRobot} />
+                          <span>{data.android_uninstall}</span>
+                        </div>
+                        <div className="space-x-2">
+                          <FontAwesomeIcon icon={faAppleAlt} />
+                          <span>{data.ios_uninstall}</span>
+                        </div>
+                      </div>
 
-                  <div className="flex flex-col items-baseline w-[80px]">
-                    <div className="space-x-2">
-                      <FontAwesomeIcon icon={faRobot} />
-                      <span>{data.android_churn}%</span>
+                      <span className="w-[30px]">{data.totalchurn}</span>
+
+                      <div className="flex flex-col items-baseline w-[80px]">
+                        <div className="space-x-2">
+                          <FontAwesomeIcon icon={faRobot} />
+                          <span>{data.android_churn}%</span>
+                        </div>
+                        <div className="space-x-2">
+                          <FontAwesomeIcon icon={faAppleAlt} />
+                          <span>{data.ios_churn}%</span>
+                        </div>
+                      </div>
                     </div>
-                    <div className="space-x-2">
-                      <FontAwesomeIcon icon={faAppleAlt} />
-                      <span>{data.ios_churn}%</span>
-                    </div>
-                  </div>
-                  
-                </div>
-                <hr className="mt-2 bg-gray-400" />
-                </>
-                
-              );
-            })}
-              
+                    <hr className="mt-2 bg-gray-400" />
+                  </>
+                );
+              })}
+            </div>
           </div>
-
+          <div className="flex justify-end p-3">
+            <>
+              {/* <Items currentItems={currentItems} /> */}
+              <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                activeLinkClassName="text-red"
+                renderOnZeroPageCount={null}
+                className="flex gap-4 text-white  mt-4 bg-green-600 "
+                id="paginate"
+              />
+            </>
           </div>
-        <div className="flex justify-end p-3">
-        <>
-      {/* <Items currentItems={currentItems} /> */}
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< previous"
-        activeLinkClassName="text-red"
-        renderOnZeroPageCount={null}
-        className ="flex gap-4 text-white  mt-4 bg-green-600 "
-        id="paginate"
-      />
-    </>
-        </div>
         </div>
       </div>
     </div>
